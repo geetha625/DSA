1dbf0e049305e0a9679d23f8c8c0ce22f934fcaf
geetha625 <geethagandham07@gmail.com>
2026-06-19 02:22:38 +0530

    added first and last occurences to bin search

--- searching/2.binary search.py
+++ searching/2.binary search.py
@@ -33,7 +33,44 @@ res=bin_search(arr,target)
 if res!=-1:
     print("element is found at : ",res)
 else:
-    print("element not found")
+    print("element not found")                      # element is found at :  4
+
+
+def first_occurrence(arr, k):
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==k:
+            ans=mid
+            right=mid-1
+        elif arr[mid]<k:
+            left=mid+1
+        else:
+            right=mid-1
+    return ans
+arr=[1,2,2,2,3,4]
+k=2
+res=first_occurrence(arr, k)
+print(res)                                  # 1
+
+def last_occurence(arr,k):
+    left=0
+    right=len(arr)-1
+    while left<=right:
+        mid=(left+right)//2
+        if arr[mid]==k:
+            ans=mid
+            left=mid+1
+        elif arr[mid]<k:
+            left=mid+1
+        else:
+            right=mid-1
+    return ans
+arr=[1,2,2,2,3,4]
+k=2
+res=last_occurence(arr,k)
+print(res)
 
 
 
