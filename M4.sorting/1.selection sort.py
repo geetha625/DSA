e49190022ecca7b8f92b2997df8253c2078caee8
geetha625 <geethagandham07@gmail.com>
2026-06-22 08:38:36 +0530

    added code

--- M4.sorting/1.selection sort.py
+++ M4.sorting/1.selection sort.py
@@ -5,4 +5,28 @@ for i in range(len(arr)):
         if arr[j]<arr[min_index]:
            min_index=j
     arr[i],arr[min_index]=arr[min_index],arr[i]
+print(arr)                                            # [2, 5, 7, 9]
+
+arr = [64, 25, 12, 22, 11]
+for i in range(len(arr)):
+    min_index=i
+    for j in range(i+1,len(arr)):
+        if arr[j]<arr[min_index]:
+            min_index=j
+    arr[i],arr[min_index]=arr[min_index],arr[i]
 print(arr)
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
+                              
