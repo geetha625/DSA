bbb59c3ea4d3c3a8d46f852a40a7fd64650b6f4c
geetha625 <geethagandham07@gmail.com>
2026-07-02 15:39:24 +0530

    solved longest substring of non repeating chars

--- M4.sorting/5.quick sort.py
+++ M4.sorting/5.quick sort.py
@@ -11,3 +11,15 @@ def quickSort(arr):
             right.append(num)
     return quickSort(left)+[pivot]+(right)
 print(quickSort([7,2,9,1,5,8]))                     # [1, 2, 5, 7, 8, 9]
+
+s = "abba"
+left=0
+seen=set()
+max_length=0
+for right in range(len(s)):
+    while s[right] in seen:
+        seen.remove(s[left])
+        left+=1
+    seen.add(s[right])
+    max_length=max(max_length,right-left+1)
+print(max_length)
