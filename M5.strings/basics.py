6d421b74537886a1dce210a1b1d2f322a01d7407
geetha625 <geethagandham07@gmail.com>
2026-07-14 07:51:01 +0530

    strings basics

---
+++ M5.strings/basics.py
@@ -0,0 +1,36 @@
+# reverse the string
+
+s="python"
+rev=""
+for ch in s:
+    rev=ch+rev
+print(rev)         # nohtyp
+
+# check palindrome
+
+t="madam"
+rev=""
+org=t
+for ch in t:
+    rev=ch+rev
+if rev==org:
+    print(True)
+else:
+    print(False)       # True
+
+# character frequency
+
+a="banana"
+freq={}
+for ch in a:
+    freq[ch]=freq.get(ch,0)+1
+print(freq)                    # {'b': 1, 'a': 3, 'n': 2}
+
+a="banana"
+freq={}
+for ch in a:
+    if ch in freq:
+        freq[ch]+=1
+    else:
+        freq[ch]=1
+print(freq)
