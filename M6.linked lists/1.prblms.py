645cbcf795e9e45aa1b3dab541952b1d80f600c7
geetha625 <geethagandham07@gmail.com>
2026-07-22 07:11:22 +0530

    traversal

--- M6.linked lists/1.prblms.py
+++ M6.linked lists/1.prblms.py
@@ -12,4 +12,30 @@ class Node:
 node1=Node(10)
 node2=Node(20)
 node1.next=node2
-print(node1.next.data)
+print(node1.next.data)    # 10 20
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+node1=Node(10)
+node2=Node(20)
+node3=Node(30)
+node1.next=node2
+node2.next=node3
+print(node1.next.next.data)         # 10 20 30
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+node1=Node(10)
+node2=Node(20)
+node3=Node(30)
+node1.next=node2
+node2.next=node3
+head=node1
+current=head
+while current:
+    print(current.data)
+    current=current.next
